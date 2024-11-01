bd2c704d2da919d9ab191f0377361b87d15e7475
Ben Thiam <seydinat235@gmail.com>
2024-11-01 14:49:53 +0000

    Mise en place des routes

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/connexion" element={<SignIn />} />
+        <Route path="/inscription" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ Front-End/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="text-3xl bg-yellow-300 text-center p-3">Home</div>;
+};
+
+export default Home;
