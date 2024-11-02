ff85f5d7b2c2ca39ed9035e6585a46ab4aa65fca
Ben Thiam <seydinat235@gmail.com>
2024-11-02 01:55:18 +0000

    Mise en place de l'api d'inscription

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import connectDB from "./database/connect.js";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
 const port = 3000;
 
 dotenv.config();
 const app = express();
 
+//Middleware
+app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRouter);
+
+//Start server
 const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
