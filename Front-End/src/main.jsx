2dc06cdf3e4a2315a24a4cb1b6be4a273b5f287a
Ben Thiam <seydinat235@gmail.com>
2024-11-04 16:25:37 +0000

    Mise en place du redux persiste

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
